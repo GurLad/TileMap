e922e63fe56e170eb978113ac6a2289ea70fcfdc
GurLad <40135047+GurLad@users.noreply.github.com>
2018-06-28 19:53:51 +0300

    Added first version of the Tile and Tilemap classes
    Added first version of the Tile and Tilemap classes

---
+++ TilemapLibrary/Tile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace TilemapLibrary
+{
+    public class Tile
+    {
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int Size;
+        public List<String> Attributes { get; set; } = new List<string>();
+        public List<Image> Images { get; set; } = new List<Image>();
+        public Color DefultColor = Color.White;
+        public Tile(int XPos, int YPos, int Size, Color DefultColor)
+        {
+            this.XPos = XPos;
+            this.YPos = YPos;
+            this.Size = Size;
+            this.DefultColor = DefultColor;
+        }
+        public Tile()
+        {
+            this.XPos = 0;
+            this.YPos = 0;
+            this.Size = 0;
+        }
+        public bool SeekAttribute(string Attribute)
+        {
+            foreach (var item in Attributes)
+            {
+                if (item == Attribute) return true;
+            }
+            return false;
+        }
+        public void SetAttribute<T>(string Attribute, T Value)
+        {
+            if (Value == null) Value = (T)Convert.ChangeType(0, typeof(T));
+            for (int i = 0; i < Attributes.Count; i++)
+            {
+                string[] Temp = Attributes[i].Split(':');
+                if (Temp.Length <= 1) continue;
+                Temp[1] = Value + "";
+                Attributes[i] = String.Join(":",Temp);
+                return;
+            }
+            Attributes.Add(Attribute + ":" + Value);
+        }
+        public string GetAttribute(string Attribute)
+        {
+            return GetAttribute<string>(Attribute);
+            /*for (int i = 0; i < Attributes.Count; i++)
+            {
+                string[] Temp = Attributes[i].Split(':');
+                if (Temp.Length <= 1) continue;
+                return Temp[1];
+            }
+            Attributes.Add(Attribute + ":0");
+            return "0";*/
+        }
+        public T GetAttribute<T>(string Attribute)
+        {
+            for (int i = 0; i < Attributes.Count; i++)
+            {
+                string[] Temp = Attributes[i].Split(':');
+                if (Temp.Length <= 1) continue;
+                return (T)Convert.ChangeType(Temp[1], typeof(T));
+            }
+            Attributes.Add(Attribute + ":0");
+            return (T)Convert.ChangeType("0", typeof(T));
+        }
+        public override string ToString()
+        {
+            return XPos + ", " + YPos + ", " + Size + ", " + DefultColor;
+        }
+    }
+}
---
+++ TilemapLibrary/Tilemap.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Reflection;
+
+namespace TilemapLibrary
+{
+    public class Tilemap
+    {
+        public int TileSize { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public Panel Parent { get; }
+        public Tile[,] Tiles { get; set; } = new Tile[99, 99];
+        public Tilemap(int Width, int Height, int TileSize, Color DefultColor, Panel Parent)
+        {
+            typeof(Panel).InvokeMember("DoubleBuffered",
+            BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+            null, Parent, new object[] { true });
+            this.TileSize = TileSize;
+            this.Width = Width;
+            this.Height = Height;
+            this.Parent = Parent;
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    Tiles[i, j] = new Tile(i, j, TileSize, DefultColor);
+                }
+            }
+            DrawTilemap();
+        }
+        public void DrawTilemap()
+        {
+            Bitmap Result = new Bitmap(Parent.Width, Parent.Height);
+            Graphics WorkWith = Graphics.FromImage(Result);
+            Pen ThePen = new Pen(Color.AliceBlue, 1);
+            SolidBrush TheBrush = new SolidBrush(Color.AliceBlue);
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    ThePen.Color = Tiles[i, j].DefultColor;
+                    TheBrush.Color = Tiles[i, j].DefultColor;
+                    WorkWith.FillRectangle(TheBrush,i*TileSize,j*TileSize,TileSize,TileSize);
+                    foreach (var item in Tiles[i, j].Images)
+                    {
+                        WorkWith.DrawImage(item, i * TileSize, j * TileSize,TileSize,TileSize);
+                    }
+                }
+            }
+            Parent.BackgroundImage = Result;
+        }
+        public bool SeekAttribute(int x, int y, string Attribute)
+        {
+            return Tiles[x, y].SeekAttribute(Attribute);
+        }
+        public void SetAttribute<T>(int x, int y, string Attribute, T Value)
+        {
+            Tiles[x, y].SetAttribute(Attribute, Value);
+        }
+        public string GetAttribute(int x, int y, string Attribute)
+        {
+            return Tiles[x, y].GetAttribute(Attribute);
+        }
+        public T GetAttribute<T>(int x, int y, string Attribute)
+        {
+            return Tiles[x, y].GetAttribute<T>(Attribute);
+        }
+    }
+}
